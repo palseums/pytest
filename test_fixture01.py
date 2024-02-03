35c289d2581dc93c5b6d30fd2b5b9af550cfbb43
palseums <57275214+palseums@users.noreply.github.com>
2024-02-03 13:18:56 -0600

    Update test_fixture01.py

--- test_fixture01.py
+++ test_fixture01.py
@@ -10,6 +10,4 @@ def test_getItem(setup_list):
     assert setup_list[0] == 'New york'
     assert setup_list[::2] == ['New york','Riyadh','mumbai']
 
-@pytest.mark.usefixtures("setup_list")
-def test_usefixturedemo():
-    assert setup_list == ['New york','London','Riyadh','singapore','mumbai']
+
